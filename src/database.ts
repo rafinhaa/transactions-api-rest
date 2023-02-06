05be52715fece14e6482ee5f6ac4472a9b100413
Rafinhaa <rafinhaa@me.com>
2023-02-06 19:18:23 -0300

    Estrutura da aplicação - Variáveis de ambiente

--- src/database.ts
+++ src/database.ts
@@ -1,14 +1,18 @@
+import "dotenv/config";
 import { knex as setupKnex, Knex } from "knex";
 
+if (!process.env.DATABASE_CLIENT || !process.env.DATABASE_URL)
+  throw new Error("Database config not found, please init your dotenv file");
+
 export const config: Knex.Config = {
-  client: "sqlite",
+  client: process.env.DATABASE_CLIENT,
   connection: {
     filename: "./database/app.db",
   },
   useNullAsDefault: true,
   migrations: {
     extension: "ts",
-    directory: "./database/migrations",
+    directory: process.env.DATABASE_URL,
   },
 };
 
