625f156a94aceaa14fdc1400499f738c76780b81
Rafinhaa <rafinhaa@me.com>
2023-02-09 16:44:26 -0300

    Testes automatizados - Finalizando testes da aplicação

--- __test__/transactions.spec.ts
+++ __test__/transactions.spec.ts
@@ -50,4 +50,64 @@ describe("app", () => {
       }),
     ]);
   });
+
+  it("should be able to get specific transaction", async () => {
+    const createTransactionResponse = await supertest(app.server)
+      .post("/transactions")
+      .send({
+        title: "abc",
+        amount: 5000,
+        type: "credit",
+      });
+
+    const cookies = createTransactionResponse.get("Set-Cookie");
+
+    const response = await supertest(app.server)
+      .get("/transactions")
+      .set("Cookie", cookies);
+
+    const transactionId = response.body.transactions[0].id;
+
+    const getTransactionResponse = await supertest(app.server)
+      .get(`/transactions/${transactionId}`)
+      .set("Cookie", cookies);
+
+    expect(getTransactionResponse.statusCode).toEqual(200);
+    expect(getTransactionResponse.body.transactions).toEqual(
+      expect.objectContaining({
+        title: "abc",
+        amount: 5000,
+      })
+    );
+  });
+
+  it("should be able to get the summary", async () => {
+    const createTransactionResponse = await supertest(app.server)
+      .post("/transactions")
+      .send({
+        title: "Credit",
+        amount: 5000,
+        type: "credit",
+      });
+
+    const cookies = createTransactionResponse.get("Set-Cookie");
+
+    await supertest(app.server)
+      .post("/transactions")
+      .set("Cookie", cookies)
+      .send({
+        title: "Debit",
+        amount: 2000,
+        type: "debit",
+      });
+
+    const summary = await supertest(app.server)
+      .get("/transactions/summary")
+      .set("Cookie", cookies);
+
+    expect(summary.statusCode).equals(200);
+    expect(summary.body.summary).toEqual({
+      amount: 3000,
+    });
+  });
 });
